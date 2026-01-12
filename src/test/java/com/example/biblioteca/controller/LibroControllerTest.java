f32875514457f5ec522920d42ebb65c11e4182ad
Moreno <diegosmh83@educastur.es>
2026-01-12 19:50:41 +0100

    MockBean

--- src/test/java/com/example/biblioteca/controller/LibroControllerTest.java
+++ src/test/java/com/example/biblioteca/controller/LibroControllerTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
@@ -27,7 +28,7 @@ class LibroControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Mock
+    @MockBean
     private LibroService servicio;
 
 
