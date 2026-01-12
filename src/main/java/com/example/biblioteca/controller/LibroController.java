5f605d604ade6ee6a7bdaa413e2a7c0516c82a8d
Moreno <diegosmh83@educastur.es>
2026-01-12 20:38:38 +0100

    Modificacion Controller

--- src/main/java/com/example/biblioteca/controller/LibroController.java
+++ src/main/java/com/example/biblioteca/controller/LibroController.java
@@ -4,6 +4,7 @@ import com.example.biblioteca.entity.Libro;
 import com.example.biblioteca.service.LibroService;
 import com.example.biblioteca.service.LibroServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,28 +15,29 @@ import java.util.Optional;
 @RequestMapping(path = "/api")
 public class LibroController {
 
+    @Autowired
+    private LibroService libroService;
 
-        @Autowired
-        private LibroService libroService;; // Se inyecta la dependencia
+    @GetMapping
+    public List<Libro> obtenerTodos() {
+        return this.libroService.getLibros();
+    }
 
-        @GetMapping
-        public List<Libro> obtenerTodos() {
-            return this.libroService.getLibros();
-        }
+    @GetMapping("/{id}")
+    public ResponseEntity<Libro> obtenerPorId(@PathVariable Long id) {
+        return libroService.getLibro(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
 
-        @GetMapping("/{id}")
-        public Optional<Libro> obtenerPorId(@PathVariable Long id) {
-            return this.libroService.getLibro(id);
-        }
+    @PostMapping
+    public void guardar0ActualizarLibros(@RequestBody Libro libro) {
+        this.libroService.guardar0ActualizarLibros(libro);
+    }
 
-        @PostMapping
-        public void guardar0ActualizarLibros(@RequestBody Libro libro) {
-             this.libroService.guardar0ActualizarLibros(libro);
-        }
-
-        @DeleteMapping("/{id}")
-        public void eliminar(@PathVariable("id") Long id) {
-            this.libroService.eliminarLibro(id);
-        }
-
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable("id") Long id) {
+        this.libroService.eliminarLibro(id);
+    }
 }
+
